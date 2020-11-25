14d3e7412bf0c0877cad73e9b3d49e0bca981fc7
Marco Tini <marcotini29@gmail.com>
2020-11-25 20:22:26 +0100

    Application supports iTunes file sharing and supports opening documents in place

--- Example/LQDownload/ViewController.swift
+++ Example/LQDownload/ViewController.swift
@@ -10,91 +10,90 @@ import UIKit
 import LQDownloadManager
 
 class ViewController: UIViewController {
-  
-  // 进度条
-  fileprivate lazy var progressView: UIProgressView = {
-    let screen = UIScreen.main.bounds
-    let rect = CGRect(x: 0, y: screen.height/2, width: screen.width, height: 2)
-    let progressView = UIProgressView(frame: rect)
     
-    progressView.trackTintColor = UIColor.gray
-    progressView.progressTintColor = UIColor.cyan
-    return progressView
-  }()
-  
-  // 下载按钮
-  fileprivate lazy var downloadButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("开始下载", for: .normal)
-    button.setTitleColor(.black, for: .normal)
-    button.layer.borderColor = UIColor.black.cgColor
-    button.layer.borderWidth = 2
-    button.layer.cornerRadius = 16
-    button.frame.size = CGSize(width: 80, height: 40)
-    let screen = UIScreen.main.bounds
-    button.center = CGPoint(x: screen.width/2, y: screen.height/2 - 80)
-    button.addTarget(self, action: #selector(downloadButtonAction), for: .touchUpInside)
-    return button
-  }()
-  
-  // 删除按钮
-  fileprivate lazy var cancelButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("取消下载", for: .normal)
-    button.setTitleColor(.black, for: .normal)
-    button.layer.borderColor = UIColor.black.cgColor
-    button.layer.borderWidth = 2
-    button.layer.cornerRadius = 16
-    button.frame.size = CGSize(width: 80, height: 40)
-    let screen = UIScreen.main.bounds
-    button.center = CGPoint(x: screen.width/2, y: screen.height/2 + 80)
-    button.addTarget(self, action: #selector(cancelButtonAction), for: .touchUpInside)
-    return button
-  }()
-  
-  
-  
-  let urlString = "https://itunesconnect.apple.com/apploader/ApplicationLoader_3.0.dmg"
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    // 进度条
+    fileprivate lazy var progressView: UIProgressView = {
+        let screen = UIScreen.main.bounds
+        let rect = CGRect(x: 0, y: screen.height/2, width: screen.width, height: 2)
+        let progressView = UIProgressView(frame: rect)
+        
+        progressView.trackTintColor = UIColor.gray
+        progressView.progressTintColor = UIColor.cyan
+        return progressView
+    }()
     
-    view.addSubview(progressView)
-    view.addSubview(downloadButton)
-    view.addSubview(cancelButton)
+    // 下载按钮
+    fileprivate lazy var downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("start download", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 16
+        button.frame.size = CGSize(width: 200, height: 40)
+        let screen = UIScreen.main.bounds
+        button.center = CGPoint(x: screen.width/2, y: screen.height/2 - 80)
+        button.addTarget(self, action: #selector(downloadButtonAction), for: .touchUpInside)
+        return button
+    }()
     
-    debugPrint("\(LQDownloadManager.shared.downloadDirectory())")
-  }
-  
+    // 删除按钮
+    fileprivate lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("cancel download", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 16
+        button.frame.size = CGSize(width: 200, height: 40)
+        let screen = UIScreen.main.bounds
+        button.center = CGPoint(x: screen.width/2, y: screen.height/2 + 80)
+        button.addTarget(self, action: #selector(cancelButtonAction), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    
+    let urlString = "https://developer.apple.com/accessories/Accessory-Design-Guidelines.pdf"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(progressView)
+        view.addSubview(downloadButton)
+        view.addSubview(cancelButton)
+        
+        debugPrint("\(LQDownloadManager.shared.downloadDirectory())")
+    }
+    
 }
 
 extension ViewController {
-  func cancelButtonAction() {
-    LQDownloadManager.shared.cancelDownload(urlString)
-    downloadButton.setTitle("开始下载", for: .normal)
-    progressView.setProgress(0, animated: true)
-  }
-  
-  func downloadButtonAction() {
-    LQDownloadManager.shared.download("https://itunesconnect.apple.com/apploader/ApplicationLoader_3.0.dmg", progress: { progress in
-      print("progress: \(progress)")
-      self.progressView.progress = Float(progress)
-    }) { status in
-      switch status {
-      case .start:
-        print("开始下载")
-        self.downloadButton.setTitle("暂停下载", for: .normal)
-      case .suspend:
-        print("下载暂停")
-        self.downloadButton.setTitle("开始下载", for: .normal)
-      case .complete:
-        print("下载完成")
-        self.downloadButton.setTitle("下载完成", for: .normal)
-      case .failed:
-        print("下载失败")
-        self.downloadButton.setTitle("重新下载", for: .normal)
-      }
+    @objc func cancelButtonAction() {
+        LQDownloadManager.shared.cancelDownload(urlString)
+        downloadButton.setTitle("start download", for: .normal)
+        progressView.setProgress(0, animated: true)
     }
-  }
+    
+    @objc func downloadButtonAction() {
+        LQDownloadManager.shared.download("https://developer.apple.com/accessories/Accessory-Design-Guidelines.pdf", progress: { progress in
+            print("progress: \(progress)")
+            self.progressView.progress = Float(progress)
+        }) { status in
+            switch status {
+            case .start:
+                print("start")
+                self.downloadButton.setTitle("pause download", for: .normal)
+            case .suspend:
+                print("suspend")
+                self.downloadButton.setTitle("start download", for: .normal)
+            case .complete:
+                print("complete")
+                self.downloadButton.setTitle("download completed", for: .normal)
+            case .failed:
+                print("failed")
+                self.downloadButton.setTitle("download again", for: .normal)
+            }
+        }
+    }
 }
-
--- Example/Pods/LQDownloadManager/LQDownloadManager/LQDownloadManager.swift
+++ Example/Pods/LQDownloadManager/LQDownloadManager/LQDownloadManager.swift
@@ -11,274 +11,299 @@ import UIKit
 
 
 open class LQDownloadManager: NSObject {
-  
-  // 下载状态
-  public enum LQDownloadState {
-    case start
-    case suspend
-    case complete
-    case failed
-  }
-  
-  struct LQDownloadModel {
-    var url: String
-    var downloadTask: URLSessionDownloadTask
-    var progressBlock: (CGFloat) -> Void
-    var completeBlock: (LQDownloadState) -> Void
-    var fileExpectedSize: Int?
-  }
-  
-  public static let shared = LQDownloadManager()
-  
-  public var directoryComponent = "/Documents/Download"
-  public var totalLengthComponent = "/Documents/Download/totalLength.plist"
-  
-  var session = URLSession()
-  
-  var onGoingDownloads = [String: LQDownloadModel]()
-  
-  public override init() {
-    super.init()
-    let configuration = URLSessionConfiguration.default
-    self.session = URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-  }
+    
+    // 下载状态
+    public enum LQDownloadState {
+        case start
+        case suspend
+        case complete
+        case failed
+    }
+    
+    struct LQDownloadModel {
+        var url: String
+        var downloadTask: URLSessionDownloadTask
+        var progressBlock: (CGFloat) -> Void
+        var completeBlock: (LQDownloadState) -> Void
+        var fileExpectedSize: Int?
+    }
+    
+    public static let shared = LQDownloadManager()
+    
+    public var directoryComponent = "/Documents"
+    public var totalLengthComponent = "/Documents/totalLength.plist"
+    
+    fileprivate lazy var session = URLSession()
+    
+    var onGoingDownloads = [String: LQDownloadModel]()
+    
 }
 
 /// session delegate
 extension LQDownloadManager: URLSessionDownloadDelegate, URLSessionDataDelegate {
-  
-  /// 下载完成 或 失败
-  public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
     
-    guard let urlString = downloadTask.originalRequest?.url?.absoluteString else {
-      return
-    }
-    guard let downloadModel = onGoingDownloads[urlString] else {
-      return
-    }
-    guard let response = downloadTask.response as? HTTPURLResponse else {
-      return
-    }
-    
-    if response.statusCode >= 400 {
-      OperationQueue.main.addOperation {
-        print("status Code: \(response.statusCode)")
-        downloadModel.completeBlock(.failed)
-      }
-      return
-    }
-    
-    onGoingDownloads.removeValue(forKey: urlString)
-    
-    do {
-      try FileManager.default.moveItem(at: location, to: URL.init(fileURLWithPath: fileFullPath(urlString)))
-    } catch {
-      OperationQueue.main.addOperation {
-        downloadModel.completeBlock(.failed)
-      }
-      return
-    }
-    
-    OperationQueue.main.addOperation {
-      downloadModel.completeBlock(.complete)
-    }
-  }
-  
-  /// 下载失败
-  public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-    guard let urlString = task.originalRequest?.url?.absoluteString else {
-      return
-    }
-    guard let downloadModel = onGoingDownloads[urlString] else {
-      return
+    /// 下载完成 或 失败
+    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        
+        guard let urlString = downloadTask.originalRequest?.url?.absoluteString else {
+            return
+        }
+        guard let downloadModel = onGoingDownloads[urlString] else {
+            return
+        }
+        guard let response = downloadTask.response as? HTTPURLResponse else {
+            return
+        }
+        
+        if response.statusCode >= 400 {
+            OperationQueue.main.addOperation {
+                print("status Code: \(response.statusCode)")
+                downloadModel.completeBlock(.failed)
+            }
+            return
+        }
+        
+        onGoingDownloads.removeValue(forKey: urlString)
+        
+        do {
+            try FileManager.default.moveItem(at: location, to: URL.init(fileURLWithPath: fileFullPath(urlString)))
+        } catch {
+            OperationQueue.main.addOperation {
+                downloadModel.completeBlock(.failed)
+            }
+            return
+        }
+        
+        OperationQueue.main.addOperation {
+            downloadModel.completeBlock(.complete)
+        }
     }
     
-    OperationQueue.main.addOperation {
-      downloadModel.completeBlock(.failed)
-    }
-    onGoingDownloads.removeValue(forKey: urlString)
-  }
-  
-  /// 更新下载进度
-  public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-    
-    guard let urlString = downloadTask.originalRequest?.url?.absoluteString else {
-      return
-    }
-    guard var downloadModel = onGoingDownloads[urlString] else {
-      return
-    }
-    
-    //存储总长度
-    if downloadModel.fileExpectedSize == nil {
-      
-      downloadModel.fileExpectedSize = Int(totalBytesExpectedToWrite)
-      
-      var dict = NSMutableDictionary(contentsOf: URL(fileURLWithPath: totalLengthFullPath()))
-      if dict == nil {
-        dict = NSMutableDictionary()
-      }
-      dict?[fileName(urlString)] = Int(totalBytesExpectedToWrite)
-      dict?.write(toFile: totalLengthFullPath(), atomically: true)
+    /// 下载失败
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        guard let urlString = task.originalRequest?.url?.absoluteString else {
+            return
+        }
+        guard let downloadModel = onGoingDownloads[urlString] else {
+            return
+        }
+        
+        OperationQueue.main.addOperation {
+            downloadModel.completeBlock(.failed)
+        }
+        onGoingDownloads.removeValue(forKey: urlString)
     }
     
-    OperationQueue.main.addOperation {
-      let progress = CGFloat(totalBytesWritten)/CGFloat(totalBytesExpectedToWrite)
-      downloadModel.progressBlock(progress)
+    /// 更新下载进度
+    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        guard let urlString = downloadTask.originalRequest?.url?.absoluteString else {
+            return
+        }
+        guard var downloadModel = onGoingDownloads[urlString] else {
+            return
+        }
+        
+        //存储总长度
+        if downloadModel.fileExpectedSize == nil {
+            
+            downloadModel.fileExpectedSize = Int(totalBytesExpectedToWrite)
+            
+            var dict = NSMutableDictionary(contentsOf: URL(fileURLWithPath: totalLengthFullPath()))
+            if dict == nil {
+                dict = NSMutableDictionary()
+            }
+            dict?[fileName(urlString)] = Int(totalBytesExpectedToWrite)
+            dict?.write(toFile: totalLengthFullPath(), atomically: true)
+        }
+        
+        OperationQueue.main.addOperation {
+            let progress = CGFloat(totalBytesWritten)/CGFloat(totalBytesExpectedToWrite)
+            downloadModel.progressBlock(progress)
+        }
+        
     }
     
-  }
-  
-  
-  
+    public func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        
+    }
+    
 }
 
 /// Public func
 extension LQDownloadManager {
-  
-  // 开始下载, 或者暂停下载
-  public func download(
-    _ urlString: String,
-    progress: @escaping (_ progress: CGFloat) -> Void,
-    completeBlock: @escaping (LQDownloadState) -> Void
+    
+    // 开始下载, 或者暂停下载
+    public func download(
+        _ urlString: String,
+        progress: @escaping (_ progress: CGFloat) -> Void,
+        completeBlock: @escaping (LQDownloadState) -> Void
     ) {
-    
-    if isComplate(urlString) {
-      completeBlock(.complete)
-      debugPrint("(￣.￣)该资源已下载完成")
-      return
+        
+        if isComplate(urlString) {
+            completeBlock(.complete)
+            debugPrint("(￣.￣)该资源已下载完成")
+            return
+        }
+        
+        //任务已存在 执行 继续或暂停
+        if let task = self.onGoingDownloads[urlString]?.downloadTask {
+            handle(urlString, task)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        session = createBackgroundSession()
+        
+        let downloadTask = session.downloadTask(with: url)
+        let downloadModel = LQDownloadModel(url: urlString, downloadTask: downloadTask, progressBlock: progress, completeBlock: completeBlock, fileExpectedSize: nil)
+        
+        downloadModel.completeBlock(.start)
+        onGoingDownloads[urlString] = downloadModel
+        downloadTask.resume()
     }
     
-    //任务已存在 执行 继续或暂停
-    if let task = self.onGoingDownloads[urlString]?.downloadTask {
-      handle(urlString, task)
-      return
+    /// 取消下载
+    public func cancelDownload(_ urlString: String) {
+        let state = isInDownloadQueue(urlString)
+        if state.0 {
+            guard let downloadModel = state.1 else {
+                return
+            }
+            downloadModel.downloadTask.cancel()
+            onGoingDownloads.removeValue(forKey: urlString)
+        }
+        // 删除可能存在的文件
+        try? FileManager.default.removeItem(atPath: fileFullPath(urlString))
     }
     
-    guard let url = URL(string: urlString) else {
-      return
+    // 删除对应的文件
+    public func deleteDownloadFile(_ urlString: String) {
+        try? FileManager.default.removeItem(atPath: fileFullPath(urlString))
     }
     
-    let downloadTask = session.downloadTask(with: url)
-    let downloadModel = LQDownloadModel(url: urlString, downloadTask: downloadTask, progressBlock: progress, completeBlock: completeBlock, fileExpectedSize: nil)
-    
-    downloadModel.completeBlock(.start)
-    onGoingDownloads[urlString] = downloadModel
-    downloadTask.resume()
-  }
-  
-  /// 取消下载
-  public func cancelDownload(_ urlString: String) {
-    let state = isInDownloadQueue(urlString)
-    if state.0 {
-      guard let downloadModel = state.1 else {
-        return
-      }
-      downloadModel.downloadTask.cancel()
-      onGoingDownloads.removeValue(forKey: urlString)
+    //判断该资源是否下载完成
+    public func isComplate(_ url: String) -> Bool {
+        guard let size = fileTotalSize(url) else { return false }
+        if size == fileDownloadSize(url) {
+            return true
+        }
+        return false
     }
-    // 删除可能存在的文件
-    try? FileManager.default.removeItem(atPath: fileFullPath(urlString))
-  }
-  
-  //判断该资源是否下载完成
-  public func isComplate(_ url: String) -> Bool {
-    guard let size = fileTotalSize(url) else { return false }
-    if size == fileDownloadSize(url) {
-      return true
-    }
-    return false
-  }
 }
 
 /// Utils
 extension LQDownloadManager {
-  
-  //文件名
-  func fileName(_ url: String) -> String {
     
-    guard let file = url.removingPercentEncoding?.components(separatedBy: "/").last else {
-      return "文件名"
-    }
-    return file
-  }
-  
-  //文件全路径
-  func fileFullPath(_ url: String) -> String {
-    return downloadDirectory() + "/\(fileName(url))"
-  }
-  
-  //下载路径: 重新下载或者重新生成的数据放在/Documents/TTDownload里面
-  public func downloadDirectory() -> String {
-    let path = NSHomeDirectory() + directoryComponent
-    if !FileManager.default.fileExists(atPath: path) {
-      try! FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
+    
+    /// Create a Background Session
+    func createBackgroundSession() -> URLSession {
+        let nowDate = Date()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYYMMddHHMMSS"
+        let dateStr =  dateFormatter.string(from: nowDate)
+        
+        let randomNum = Int(arc4random()%10000)+1
+        
+        let configrationIdentifier =  "test" + dateStr + String(randomNum)
+        
+        
+        let backgroundSessionConfigration = URLSessionConfiguration.background(withIdentifier: configrationIdentifier)
+        
+        return URLSession(configuration: backgroundSessionConfigration, delegate: self, delegateQueue: OperationQueue.main)
     }
     
-    return path
-  }
-  
-  //获取对应资源的大小: 大小存储在totalLength.plist, key为url
-  public func fileTotalSize(_ url: String) -> Int? {
-    if let data = try? Data(contentsOf: URL(fileURLWithPath: totalLengthFullPath())){
-      if let result = try? PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any] {
-        return result?[fileName(url)] as? Int
-      }
-    }
-    return nil
-  }
-  
-  //存储文件总长度.plist的文件路径
-  func totalLengthFullPath() -> String{
-    
-    let path = NSHomeDirectory() + totalLengthComponent
-    if !FileManager.default.fileExists(atPath: path) {
-      FileManager.default.createFile(atPath: path, contents: nil, attributes: nil)
+    //文件名
+    func fileName(_ url: String) -> String {
+        
+        guard let file = url.removingPercentEncoding?.components(separatedBy: "/").last else {
+            return "文件名"
+        }
+        return file
     }
     
-    return path
-  }
-  
-  //文件已下载大小
-  func fileDownloadSize(_ url: String) -> Int {
-    var filesize: Int?
+    //文件全路径
+    func fileFullPath(_ url: String) -> String {
+        return downloadDirectory() + "/\(fileName(url))"
+    }
     
-    do {
-      let attr = try FileManager.default.attributesOfItem(atPath: fileFullPath(url))
-      filesize = attr[FileAttributeKey.size] as? Int
-    } catch {
-      return 0
+    //下载路径: 重新下载或者重新生成的数据放在/Documents/TTDownload里面
+    public func downloadDirectory() -> String {
+        let path = NSHomeDirectory() + directoryComponent
+        if !FileManager.default.fileExists(atPath: path) {
+            try! FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
+        }
+        
+        return path
     }
-    guard let size = filesize else {
-      return 0
-    }
-    return size
-  }
-  
-  
-  //开始或暂停 任务
-  func handle(_ urlString: String, _ task: URLSessionDownloadTask) {
     
-    switch task.state {
-    case .running:
-      task.suspend()
-      onGoingDownloads[urlString]?.completeBlock(.suspend)
-    case .canceling,
-         .suspended:
-      task.resume()
-      onGoingDownloads[urlString]?.completeBlock(.start)
-    case .completed:
-      break
+    //获取对应资源的大小: 大小存储在totalLength.plist, key为url
+    public func fileTotalSize(_ url: String) -> Int? {
+        if let data = try? Data(contentsOf: URL(fileURLWithPath: totalLengthFullPath())){
+            if let result = ((try? PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any]) as [String : Any]??) {
+                return result?[fileName(url)] as? Int
+            }
+        }
+        return nil
     }
-  }
-  
-  /// 是否在下载队列中
-  func isInDownloadQueue(_ urlString: String) -> (Bool, LQDownloadModel?) {
-    for (uniqueKey, downloadModel) in onGoingDownloads {
-      if urlString == uniqueKey {
-        return (true, downloadModel)
-      }
+    
+    //存储文件总长度.plist的文件路径
+    func totalLengthFullPath() -> String{
+        
+        let path = NSHomeDirectory() + totalLengthComponent
+        if !FileManager.default.fileExists(atPath: path) {
+            FileManager.default.createFile(atPath: path, contents: nil, attributes: nil)
+        }
+        
+        return path
     }
-    return (false, nil)
-  }
+    
+    //文件已下载大小
+    func fileDownloadSize(_ url: String) -> Int {
+        var filesize: Int?
+        
+        do {
+            let attr = try FileManager.default.attributesOfItem(atPath: fileFullPath(url))
+            filesize = attr[FileAttributeKey.size] as? Int
+        } catch {
+            return 0
+        }
+        guard let size = filesize else {
+            return 0
+        }
+        return size
+    }
+    
+    
+    //开始或暂停 任务
+    func handle(_ urlString: String, _ task: URLSessionDownloadTask) {
+        
+        switch task.state {
+        case .running:
+            task.suspend()
+            onGoingDownloads[urlString]?.completeBlock(.suspend)
+        case .canceling,
+             .suspended:
+            task.resume()
+            onGoingDownloads[urlString]?.completeBlock(.start)
+        case .completed:
+            break
+        @unknown default:
+            print("error")
+        }
+    }
+    
+    /// 是否在下载队列中
+    func isInDownloadQueue(_ urlString: String) -> (Bool, LQDownloadModel?) {
+        for (uniqueKey, downloadModel) in onGoingDownloads {
+            if urlString == uniqueKey {
+                return (true, downloadModel)
+            }
+        }
+        return (false, nil)
+    }
 }
