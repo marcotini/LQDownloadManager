5c71337719e39f8532970558bd6450a3abd733b3
Marco Tini <marcotini29@gmail.com>
2020-12-01 08:45:03 +0100

    No Pods

--- Example/LQDownload/AppDelegate.swift
+++ Example/LQDownload/AppDelegate.swift
@@ -10,30 +10,26 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
-	var window: UIWindow?
-
-
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
-		return true
-	}
-
-	func applicationWillResignActive(_ application: UIApplication) {}
-
-	func applicationDidEnterBackground(_ application: UIApplication) {}
-  
-  // need pr 🤣
-  func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
-    print("application session")
-  }
-
-	func applicationWillEnterForeground(_ application: UIApplication) {}
-
-	func applicationDidBecomeActive(_ application: UIApplication) {}
-
-	func applicationWillTerminate(_ application: UIApplication) {}
-
-
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {}
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+    
+    // need pr 🤣
+    func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+        print("application session")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+    
+    func applicationWillTerminate(_ application: UIApplication) {}
 }
-
